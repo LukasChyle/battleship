24159879e0f47502d0a396005c37e01a8ae8f0be
Lukas Chyle <lukas_chyle@hotmail.com>
2024-03-09 22:37:01 +0100

    handleTiles function is working and the ships are rendering on the board.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
 import {List} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {DndContext, useDraggable, useDroppable} from "@dnd-kit/core";
 import {CSS} from "@dnd-kit/utilities"
 
-function getInitialTileRows() {
-    const board = Array.apply(null, Array(10)).map(() => (
-        Array.apply(null, Array(10)).map(function () {
-        })))
+const getInitialShips = [
+    {id: "ship-1", isHorizontal: true, length: 2, row: 1, col: 1},
+    {id: "ship-2", isHorizontal: false, length: 2, row: 5, col: 5},
+    {id: "ship-3", isHorizontal: false, length: 3, row: 5, col: 6},
+    {id: "ship-4", isHorizontal: false, length: 4, row: 5, col: 7},
+    {id: "ship-5", isHorizontal: false, length: 5, row: 5, col: 8},
+]
 
-    return board.map((row, rowIndex) => ({
-        tiles: row.map((tile, columnIndex) => ({
-            id: (rowIndex + "" + columnIndex),
+const board = Array.apply(null, Array(10)).map(() => (
+    Array.apply(null, Array(10)).map(function () {
+    })))
+
+const getInitialTiles = () => {
+    const tiles = []
+    board.forEach((row, rowIndex) => (row.forEach((col, colIndex) => {
+        tiles.push({
+            id: (rowIndex + "" + colIndex),
             row: rowIndex,
-            col: columnIndex,
+            col: colIndex,
             used: false,
             ship: undefined
-        }))
-    }))
-}
-
-function getInitialShips() {
-    return [
-        {id: "ship-1", isHorizontal: true, length: 2, row: 1, col: 1},
-        {id: "ship-2", isHorizontal: false, length: 2, row: 5, col: 5},
-        {id: "ship-3", isHorizontal: false, length: 3, row: 5, col: 6},
-        {id: "ship-4", isHorizontal: false, length: 4, row: 5, col: 7},
-        {id: "ship-5", isHorizontal: false, length: 5, row: 5, col: 8},
-    ]
+        })
+    })))
+    return tiles
 }
 
 function App() {
     const [ships, setShips] = useState(getInitialShips);
-    const [tileRows, setTilesRows] = useState(getInitialTileRows)
+    const [tiles, setTiles] = useState(getInitialTiles())
     console.log(ships)
-    console.log(tileRows)
+    console.log(tiles)
 
     const handleDragStart = (e) => {
     }
@@ -53,54 +53,49 @@ function App() {
         }
     }
 
-// TODO: make method that iterates through tileRows and puts a ships value inside if col and row is matched, change used to true.
-    const handleBoats = () => {
-        // TODO: instead of iterating tiles, iterate ships and just match tile id from a map of tiles without rows?
-        // TODO: mapOf: ((row + "" + col), {tile info}) hardcode the full map and id?
-        tileRows.forEach(r => r.tiles.forEach(t => {
-            ships.forEach(s => {
-                if (s.row === t.row && s.col === t.col) {
-                    // TODO: put boat into the tile.
+    const handleTiles = () => {
+        const tilesToChange = []
+        ships.forEach((e) => {
+            for (let i = 0; i < e.length; i++) {
+                if (e.isHorizontal) {
+                    tilesToChange.push({id: (e.row + "" + (e.col + i)), ship: e.col === (e.col + i) ? e : undefined})
+                } else {
+                    tilesToChange.push({id: ((e.row - i) + "" + e.col), ship: e.row === (e.row + i) ? e : undefined})
                 }
-            })
+            }
+        })
+        setTiles(tiles.map((e) => {
+            const match = tilesToChange.find(t => t.id === e.id)
+            return match ? {...e, used: true, ship: match.ship} : e
         }))
     }
 
-// TODO: When a ship is matched to a tile: Create a method that looks at the ship and changes tiles used by the ship to used: true.
-    const handleTiles = () => {
-// TODO: tile id = (row + "" + col)
-    }
+    useEffect(() => {
+        handleTiles()
+    }, [ships]);
 
     return (
         <DndContext onDragEnd={handleDragEnd} onDragStart={handleDragStart} onDragOver={handleDragOver}>
             <div>
-                <ShipList ships={ships}/>
-                <Board tileRows={tileRows}/>
+                {/*<ShipList ships={ships}/>*/}
+                <Board board={board} tiles={tiles}/>
             </div>
         </DndContext>
     )
 }
 
-function Board({tileRows}) {
+function Board({board, tiles}) {
     return (
         <div>
-            {tileRows.map((row) => (
-                <div className="board-row" key={row.rowNumber}>
-                    {row.tiles.map((tile) => (<BoardTile key={tile.id} tile={tile}/>))}
+            {board.map((row, rowIndex) => (
+                <div className="board-row" key={rowIndex}>
+                    {row.map((col, colIndex) => (
+                        <BoardTile key={colIndex} tile={
+                        tiles.find(t => t.id === rowIndex + "" + colIndex)
+                    }/>))}
                 </div>
             ))}
         </div>
-    )
-}
-
-function BoardTile({tile}) {
-    const {setNodeRef} = useDroppable({id: tile.id, data: {row: tile.row, col: tile.col}, disabled: tile.used})
-    return (
-        <span className="board-tile" ref={setNodeRef}>
-            {tile.used && tile.ship && <Ship id={tile.ship.id} key={tile.ship.id} isHorizontal={tile.ship.isHorizontal}
-                                             length={tile.ship.length}/>}
-            <img src="/src/assets/framed-water.jpg" width={75} height={75} alt="board-tile"/>
-        </span>
     )
 }
 
@@ -112,6 +107,18 @@ function ShipList({ships}) {
                       length={ship.length}/>
             ))}
         </List>
+    )
+}
+
+function BoardTile(props) {
+    const {setNodeRef} = useDroppable({id: props.tile.id, data: {row: props.tile.row, col: props.tile.col}, disabled: props.tile.used})
+
+    return (
+        <span className="board-tile" ref={setNodeRef}>
+            {props.tile.ship && <Ship id={props.tile.ship.id} key={props.tile.ship.id} isHorizontal={props.tile.ship.isHorizontal}
+                                length={props.tile.ship.length}/>}
+            <img className="tile-img" src="/src/assets/framed-water.jpg" width={75} height={75} alt="board-tile"/>
+        </span>
     )
 }
 
@@ -162,7 +169,7 @@ function Ship(ship) {
             {...attributes}
             {...listeners}
         >
-            <img src={srcString} alt={"Ship"}/>
+            <img className="ship-img" src={srcString} alt={"Ship"}/>
         </div>
     )
 }
