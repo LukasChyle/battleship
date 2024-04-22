7721bdaf7fa67c8c6c6843d788242ee727c10f29
Lukas Chyle <lukas_chyle@hotmail.com>
2024-04-22 10:42:46 +0200

    Fixed ship images to prevent overlaps, replaced vert images with rotation.

--- src/App.jsx
+++ src/App.jsx
@@ -247,19 +247,19 @@ function Ship({id, isHorizontal, length, row, col, ships, onShips, canBeLaid, ma
     } else {
         switch (length) {
             case 2 :
-                srcString = "src/assets/ship_4_vert.png"
+                srcString = "src/assets/ship_4.png"
                 shipImageStyle = "img-ship-4-vert"
                 break
             case 3 :
-                srcString = "src/assets/ship_3_vert.png"
+                srcString = "src/assets/ship_3.png"
                 shipImageStyle = "img-ship-3-vert"
                 break
             case 4 :
-                srcString = "src/assets/ship_2_vert.png"
+                srcString = "src/assets/ship_2.png"
                 shipImageStyle = "img-ship-2-vert"
                 break
             case 5 :
-                srcString = "src/assets/ship_1_vert.png"
+                srcString = "src/assets/ship_1.png"
                 shipImageStyle = "img-ship-1-vert"
                 break
         }
