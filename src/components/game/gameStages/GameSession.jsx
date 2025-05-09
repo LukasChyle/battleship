902e93fb839d9c1e684f8a0a9f65ed4cf1430c4a
Lukas Chyle <lukas_chyle@hotmail.com>
2025-05-09 02:27:15 +0200

    improved index.html and added favicon.ico

--- src/components/game/gameStages/GameSession.jsx
+++ src/components/game/gameStages/GameSession.jsx
@@ -62,6 +62,7 @@ export default function GameSession({
         }
     }, [readyState]);
 
+    //TODO: create and add preview.png for index.html
     //TODO: Refactor tiles and ships in setUp to use row and column instead of id.
     //TODO: see if Ship do need the variable of ID, or if it can be removed before sending ships to backend.
     //TODO: in gameSetUp, test if the server is online and if not write a message box and block "start game"
